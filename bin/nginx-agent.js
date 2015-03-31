f473046bb9657a2600483b809fe1d6fcd312f9c6
Boris Okunskiy <i@savant.pro>
2015-03-31 14:14:13 +0300

    Writing root conf on start

--- bin/nginx-agent.js
+++ bin/nginx-agent.js
@@ -4,7 +4,8 @@
 var path = require('path')
   , extend = require('extend')
   , fs = require('fs-extra')
-  , http = require('http');
+  , http = require('http')
+  , NginxConf = require('nginx-json');
 
 var options = {
   port: 7390,
@@ -20,4 +21,16 @@ var app = require('../src/app')(options);
 
 http.createServer(app).listen(options.port, options.ip, function() {
   console.log('Listening on %s:%s', options.ip, options.port);
+});
+
+// Bootstrap current dir with default `nginx.conf`
+
+var rootConf = new NginxConf({ 'include': 'tenants/*/nginx.conf' })
+  , rootConfFile = path.join(process.cwd(), 'nginx.conf');
+
+fs.writeFile(rootConfFile, rootConf.toString(), 'utf-8', function(err) {
+  if (err)
+    console.error('Could not write to %s', rootConfFile);
+  else
+    console.log('Written root conf to %s', rootConfFile);
 });
